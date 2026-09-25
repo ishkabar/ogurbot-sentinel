bafea5f76d41091ce1eda0c08335113fa66770f3
ishkabar <dom.karczewski@gmail.com>
2026-09-25 13:21:49 +0200

    fix(api): allow  widget iframe via CSP frame

--- src/Ogur.Sentinel.Api/Program.cs
+++ src/Ogur.Sentinel.Api/Program.cs
@@ -122,7 +122,8 @@ try
             "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; " +
             "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; " +
             "img-src 'self' data:; " +
-            "font-src 'self';"
+            "font-src 'self'; " +
+            "frame-src 'self' https://zrzutka.pl;"
         );
         await next();
     });
